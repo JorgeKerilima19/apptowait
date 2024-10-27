96da2199a7ab17b4990d9d1ad24adcb07a33f96f
JorgeMioska <quispechilojorge@gmail.com>
2024-10-26 20:08:42 -0500

    home + navbar search work

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,6 +1,7 @@
 import { Artist, GenreCard, MusicCard } from "../components";
 import useWindowSizeStore from "../store/windowSize";
 import img from "../assets/placeholder.jpg";
+import search from "../assets/SVG/search.svg";
 
 export const Home = () => {
   const windowSize = useWindowSizeStore((state: any) => state.windowSize);
@@ -8,23 +9,35 @@ export const Home = () => {
   return (
     <>
       <div className="mb-4 flex justify-btw items-center pr-3">
-        <form action="">
-          <input type="text" id="search" />
+        <form className="flex items-center gap-4 form p-4" action="">
+          <img className="icon" src={search} alt="search" />
+          <input
+            className="search text-secondary"
+            type="text"
+            id="search"
+            placeholder="type artist or song"
+          />
         </form>
         <div className="user-container flex items-center gap-5 p-2">
           <img className="user-img" src={img} alt="User Image" />
           <span>user name</span>
         </div>
       </div>
-      <div className="flex gap-4 overflow-x-scroll music-cards-container">
-        {[...Array(8)].map((el) => (
-          <MusicCard />
-        ))}
+      <div className="separator-top py-5">
+        <h2>Today's Selection</h2>
+        <div className="flex gap-4 overflow-x-scroll py-4 music-cards-container">
+          {[...Array(8)].map((el) => (
+            <MusicCard />
+          ))}
+        </div>
       </div>
-      <div className="flex gap-4 w-full overflow-auto">
-        {[...Array(8)].map((el) => (
-          <GenreCard />
-        ))}
+      <div className="separator-top py-4">
+        <h2>Genres</h2>
+        <div className="flex gap-4 py-5 w-full overflow-auto">
+          {[...Array(8)].map((el) => (
+            <GenreCard />
+          ))}
+        </div>
       </div>
       <div className="flex-col gap-4 w-full">
         <h3>For you</h3>
